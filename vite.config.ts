0e06afbb1681cd522e5af17902b71b2c49d38c75
2452327415qh-debug <2452327415qh@gmail.com>
2026-07-09 23:29:13 +0800

    更新vite配置文件

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,7 @@ export default defineConfig({
                 ShinChan: resolve(__dirname, "example", "ShinChan.html"),
                 OfficeBuilding: resolve(__dirname, "example", "OfficeBuilding.html"),
                 shooting: resolve(__dirname, "example", "shooting", "shooting.html"),
+                legik: resolve(__dirname, "example", "legik", "legik.html"),
                 multiplayergltf: resolve(__dirname, "example", "multiplayer-gltf.html"),
                 multiplayer3dgs: resolve(__dirname, "example", "multiplayer-3dgs.html"),
             },
